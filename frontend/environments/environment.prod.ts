645c268ec7f8b2c705bcf757402b51d4295a0e3b
vishal-2022A7PS0174H <144363866+vishal-2022A7PS0174H@users.noreply.github.com>
2024-07-01 09:43:43 +0530

    Add files via upload

---
+++ frontend/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  base_url: 'http://localhost:8080/',
+
+  // dialog modal icon
+  warning: 'fa fa-exclamation-triangle',
+  error: 'fas fa-times-circle',
+  info: 'fas fa-exclamation-circle',
+
+  info_message: 'Info',
+  warning_message: 'Warning',
+  error_message: 'Error',
+};
---
+++ frontend/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  base_url: 'http://localhost:8080',
+  // base_url: 'https://e51f-103-20-55-5.ngrok-free.app',
+
+  // dialog modal icon
+  warning: 'fa fa-exclamation-triangle',
+  error: 'fa fa-times-circle',
+  info: 'fa fa-exclamation-circle',
+
+  info_message: 'Info',
+  warning_message: 'Warning',
+  error_message: 'Error',
+};
+
